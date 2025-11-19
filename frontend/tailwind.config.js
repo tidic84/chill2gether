f6e70dd33a482c63c51540a99c94822df9b2a79b
Nizyi <tomysen812@gmail.com>
2025-11-19 01:21:48 +0100

    homefonctionnel

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                gradientShift: {
+                    '0%': { backgroundPosition: '0% 50%' },
+                    '50%': { backgroundPosition: '100% 50%' },
+                    '100%': { backgroundPosition: '0% 50%' },
+                },
+            },
+            animation: {
+                gradientShift: 'gradientShift 30s ease infinite',
+            },
+            backgroundSize: {
+                '400%': '400% 400%',
+            },
+        },
     },
     plugins: [],
 }
